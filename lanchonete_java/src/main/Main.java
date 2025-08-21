1d06bddcbaa11d9b26f8dc75ead52c53c1f04d32
Waldemar <149939866+waldebr@users.noreply.github.com>
2025-08-21 13:53:11 -0300

    Add files via upload

---
+++ lanchonete_java/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dti
+ */
+public class Main {
+    
+}
